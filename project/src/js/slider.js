fc36adf384c398c4aa239780cb6205193939ec5b
daoduyhoang <46489442+daoduyhoang@users.noreply.github.com>
2020-07-17 17:15:25 +0700

    fix contact

--- project/src/js/slider.js
+++ project/src/js/slider.js
@@ -25,3 +25,32 @@ $('.slide-card').owlCarousel({
     },
     dots: true
 });
+//start slide khách hàng của Reti
+$('.khachHangCuaReti-slide').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: ["<i class='fas fa-chevron-left duannoibat-btn-prev fa-2x'></i>", "<i class='fas fa-chevron-right duannoibat-btn-next fa-2x'></i>"],
+    responsive: {
+        0: {
+            items: 1
+        },
+        480: {
+            items: 1
+        },
+        576: {
+            items: 1
+        },
+        768: {
+            items: 1
+        },
+        992: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    },
+    dots: false
+});
+  //end slide khach hang cua reti
